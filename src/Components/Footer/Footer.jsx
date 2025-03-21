0e6f56a6f05db08d45a8195369f8286cabfaf581
Lucas Nicolás Ramírez <lucaas.ramiirez94@gmail.com>
2025-03-21 01:49:52 -0300

    asdd

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
 import { Divider } from '@mui/material';
-import './Footer.css';
+import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <>
-            <div className='pieDePag'>
 
-                <div className='contactInfo'>
-                    <h3 className='tituloVirch'>VirchVape</h3>
-                    <p>Contacto: info@virchvape.com</p>
-                    <p>Teléfono: +54 9 11 1234-5678</p>
-                </div>
 
-                <Divider color="gray" sx={{ width: '100%', margin: '10px 0' }} />
+        <div className={styles.pieDePag}>
 
-                <div className='developerInfo'>
-                    <p className='developerText'> © 2025 Todos los derechos reservados</p>
-                    <p className='infoDev'>Desarrollado por</p>
-                    <a className='developerLink' href="https://portafolio-eosin-two.vercel.app" target="_blank" rel="noopener noreferrer">
-                        <p className='logoContainer'>
-                            LR.DEV
-                        </p>
-                    </a>
-                </div>
+            <div className={styles.contactInfo}>
+                <h3 className={styles.tituloVirch} >VirchVape</h3>
+                <p>Contacto: info@virchvape.com</p>
+                <p>Teléfono: +54 9 11 1234-5678</p>
             </div>
-        </>
+
+            <Divider color="gray" sx={{ width: '100%', margin: '10px 0' }} />
+
+            <p className={styles.developerText}> © 2025 Todos los derechos reservados</p>
+
+            <div className={styles.developerInfo}>
+                <p className={styles.infoDev}>Desarrollado por</p>
+                <a className={styles.developerLink} href="https://portafolio-eosin-two.vercel.app" target="_blank" rel="noopener noreferrer">
+                    <p className={styles.logoContainer}>
+                        LR.DEV
+                    </p>
+                </a>
+            </div>
+        </div>
+
     )
 }
 
