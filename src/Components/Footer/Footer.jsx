4bbe12813d668ff14428bca939830725fd60d237
Lucas Nicolás Ramírez <lucaas.ramiirez94@gmail.com>
2025-03-21 01:14:17 -0300

    act footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,19 +6,13 @@ function Footer() {
     return (
 
 
-        <footer className='footer'>
+        <div className='pieDePag'>
 
             <div className='contactInfo'>
                 <h3 className='tituloVirch'>VirchVape</h3>
                 <p>Contacto: info@virchvape.com</p>
                 <p>Teléfono: +54 9 11 1234-5678</p>
             </div>
-
-            {/* <div className={styles.footerLinks}>
-                    <a href="/terms" className={styles.footerLink}>Términos y condiciones</a>
-                    <a href="/privacy" className={styles.footerLink}>Política de privacidad</a>
-                    <a href="/contacto" className={styles.footerLink}>Contacto</a>
-                </div> */}
 
             <Divider color="gray" sx={{ width: '100%', margin: '10px 0' }} />
 
@@ -31,7 +25,7 @@ function Footer() {
                     </p>
                 </a>
             </div>
-        </footer>
+        </div>
 
     )
 }
