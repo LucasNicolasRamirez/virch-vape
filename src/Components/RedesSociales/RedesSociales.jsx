0b03b72cdae4b7e83b5f62e7ada54bb3224b6b9c
Lucas Nicolás Ramírez <lucaas.ramiirez94@gmail.com>
2025-03-25 02:40:49 -0300

    saasa

--- src/Components/RedesSociales/RedesSociales.jsx
+++ src/Components/RedesSociales/RedesSociales.jsx
@@ -5,95 +5,101 @@ import styles from './RedesSociales.module.css';
 
 const RedesSociales = () => {
     return (
-        
-            <div className={styles.socialIconsContainer}>
-                {/* INSTAGRAM */}
-                <div className={styles.tooltipContainer}>
-                    <div className={styles.tooltip}>
+
+        <div className={styles.socialIconsContainer}>
+            {/* INSTAGRAM */}
+            <div className={styles.tooltipContainer}>
+                <div className={styles.tooltip}>
+                    <a className={styles.icon} href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
                         <div className={styles.profile}>
                             <div className={styles.user}>
-                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape"/></div>
+                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape" /></div>
                                 <div className={styles.details}>
                                     <div className={styles.name}>VirchVape</div>
                                     <div className={styles.username}>@virchvape.tw</div>
                                 </div>
                             </div>
-                            
+
                         </div>
-                    </div>
-                    <div className={styles.text}>
-                        <a className={styles.icon} href="#" target="_blank" rel="noopener noreferrer">
-                            <div className={styles.layer}>
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span className={styles.instagram}>
-                                    <FaInstagram size={50} />
-                                </span>
-                            </div>
-                            <div className={styles.text}>Instagram</div>
-                        </a>
-                    </div>
+                    </a>
                 </div>
-                {/* WHATSAPP */}
-                <div className={styles.tooltipContainer}>
-                    <div className={styles.tooltip}>
+                <div className={styles.text}>
+                    <a className={styles.icon} href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <div className={styles.layer}>
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span className={styles.instagram}>
+                                <FaInstagram size={50} />
+                            </span>
+                        </div>
+                        <div className={styles.text}>Instagram</div>
+                    </a>
+                </div>
+            </div>
+            {/* WHATSAPP */}
+            <div className={styles.tooltipContainer}>
+                <div className={styles.tooltip}>
+                    <a className={styles.icon} href="https://wa.me/542804857557" target="_blank" rel="noopener noreferrer">
                         <div className={styles.profile}>
                             <div className={styles.user}>
-                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape"/></div>
+                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape" /></div>
                                 <div className={styles.details}>
                                     <div className={styles.name}>VirchVape Trelew</div>
                                     <div className={styles.username}>(280)-12345678 </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.text}>
-                        <a className={styles.icon} href="#">
-                            <div className={styles.layer}>
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span className={styles.whatsapp}>
-                                    <FaWhatsapp size={50}  />
-                                </span>
-                            </div>
-                            <div className={styles.text}>Whatsapp</div>
-                        </a>
-                    </div>
+                    </a>
                 </div>
-                {/* FACEBOOK */}
-                <div className={styles.tooltipContainer}>
-                    <div className={styles.tooltip}>
+                <div className={styles.text}>
+                    <a className={styles.icon} href="https://wa.me/542804857557" target="_blank" rel="noopener noreferrer">
+                        <div className={styles.layer}>
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span className={styles.whatsapp}>
+                                <FaWhatsapp size={50} />
+                            </span>
+                        </div>
+                        <div className={styles.text}>Whatsapp</div>
+                    </a>
+                </div>
+            </div>
+            {/* FACEBOOK */}
+            <div className={styles.tooltipContainer}>
+                <div className={styles.tooltip}>
+                    <a className={styles.icon} href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
                         <div className={styles.profile}>
                             <div className={styles.user}>
-                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape"/></div>
+                                <div className={styles.img}><img src={logo} className={styles.imagenLogo} alt="Logo VirchVape" /></div>
                                 <div className={styles.details}>
                                     <div className={styles.name}>VirchVape</div>
                                     <div className={styles.username}>@virchvape.tw</div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.text}>
-                        <a className={styles.icon} href="#">
-                            <div className={styles.layer}>
-                                <span />
-                                <span />
-                                <span />
-                                <span />
-                                <span className={styles.facebook}>
-                                    <FaFacebook size={40}  />
-                                </span>
-                            </div>
-                            <div className={styles.text}>Facebook</div>
-                        </a>
-                    </div>
+                    </a>
+                </div>
+                <div className={styles.text}>
+                    <a className={styles.icon} href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <div className={styles.layer}>
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span className={styles.facebook}>
+                                <FaFacebook size={40} />
+                            </span>
+                        </div>
+                        <div className={styles.text}>Facebook</div>
+                    </a>
                 </div>
             </div>
-      
+        </div>
+
     );
 }
 export default RedesSociales;
