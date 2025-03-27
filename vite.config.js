c26ac58f6e3bdb9bb32dce9f761bb025cde3c16a
Lucas Nicolás Ramírez <lucaas.ramiirez94@gmail.com>
2025-03-27 02:13:47 -0300

    asdf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-})
+  base: './', // Esto es útil si estás sirviendo tu aplicación desde un subdirectorio
+  server: {
+    historyApiFallback: true, // Esto ayuda a manejar el enrutamiento del lado del cliente
+  },
+});
