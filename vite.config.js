7422a1d217009522e56592b4797d24e2d901a11d
Lucas Nicolás Ramírez <lucaas.ramiirez94@gmail.com>
2025-04-04 20:48:07 -0300

    dawd

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto es útil si estás sirviendo tu aplicación desde un subdirectorio
-  server: {
-    historyApiFallback: true, // Esto ayuda a manejar el enrutamiento del lado del cliente
-  },
+  base: '/', // Esto es útil si estás sirviendo tu aplicación desde un subdirectorio
 });
